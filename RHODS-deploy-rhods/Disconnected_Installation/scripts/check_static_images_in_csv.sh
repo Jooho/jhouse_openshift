762228f1946e8db7c55d62877718f5f3a6907a4d
jooho <jlee@redhat.com>
2023-04-13 16:13:09 -0400

    update script

--- RHODS-deploy-rhods/Disconnected_Installation/scripts/check_static_images_in_csv.sh
+++ RHODS-deploy-rhods/Disconnected_Installation/scripts/check_static_images_in_csv.sh
@@ -26,27 +26,28 @@ fi
 
 cd ${test_home}
 if [[ ! -f ${test_home}/rhods-operator.clusterserviceversion.yml ]]; then
-  echo "Download CSV from rhods-cpaas-midstream"
+  log::str::print  "${info}[INFO] Download CSV from rhods-cpaas-midstream${clear}\n"
   wget -q https://gitlab.cee.redhat.com/data-hub/rhods-cpaas-midstream/-/raw/rhods-${rhods_version}-rhel-8/distgit/containers/odh-operator-bundle/bundle-template/manifests/rhods-operator.clusterserviceversion.yml >/dev/null
 fi
 
 if [[ ! -f ${test_home}/upstream_sources.yml ]]; then
-  echo "Download upstream_resources.yaml from rhods-cpaas-midstream"
+  log::str::print  "${info}[INFO] Download upstream_resources.yaml from rhods-cpaas-midstream${clear}\n"
   wget -q https://gitlab.cee.redhat.com/data-hub/rhods-cpaas-midstream/-/raw/rhods-${rhods_version}-rhel-8/upstream_sources.yml >/dev/null
 fi
 
 echo 
-echo "Clone all repositories for RHODS ${rhods_version} if it does not exist"
+log::str::print  "${info}[INFO] Clone all repositories for RHODS ${rhods_version} if it does not exist${clear}\n"
 while read -r url && read -r branch; do
   name=$(basename ${url}|cut -d. -f1)
   if [ ! -d ${test_home}/${name} ]; then
-    echo "Name: ${name}, URL: $url, Branch: $branch"
+    log::str::print  "${light_blue}- Name: ${name}, URL: $url, Branch: $branch${clear}\n"
     git clone --quiet --branch $branch $url $name >/dev/null
   fi
 done < <(yq eval '.git[] | [.url, .branch]' ${test_home}/upstream_sources.yml|sed 's/- //g')
 
 echo
-echo "[INFO] Start to check if the images in the CSV annotations are being used in any repositories"
+log::str::print "${info}[INFO] Start to check if the images in the CSV annotations are being used in any repositories${clear}"
+echo
 not_being_used_images=()
 for img in $(yq eval '.metadata.annotations | to_entries | map(select(.key | test("image"))) | .[].value' ./rhods-operator.clusterserviceversion.yml)
 do
@@ -56,19 +57,20 @@ do
     not_being_used_images+=(${img})
     echo "--> Fail"
   else
-    echo "--> Pass"
+   echo "--> Pass"
   fi
 done
 
 echo
 if [ ${#not_being_used_images[@]} -gt 0 ]; then
-  echo "[ERROR: Detect some images in csv annotations are not being used in any repositories]"
-  echo "[INFO: The following images are detected]"
+  log::str::print  "${error}[ERROR] Detect some images in csv annotations are not being used in any repositories${clear}\n"
+  log::str::print  "${error}[ERROR] The following images are detected${clear}\n"
   for element in "${not_being_used_images[@]}"
   do
-    echo "- ${element}"
+    log::str::print  "${light_red}- ${element}\n"
   done
 else
-  echo "[SUCCESS: All images in csv annotations are being used in RHODS repositories]"
+  log::str::print  "${green}[green] All images in csv annotations are being used in RHODS repositories"
+  echo
 fi
 
